afe91dafd9b1f3793a405eab2021adf0552f73c9
whkj_zhengxiangyi <2830046752@qq.com>
2025-04-28 11:28:10 +0800

    3

---
+++ src/main/java/com/xi/controller/OrderItemController.java
@@ -0,0 +1,18 @@
+package com.xi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 订单项 前端控制器
+ * </p>
+ *
+ * @author 郑西
+ * @since 2025-04-28
+ */
+@RestController
+@RequestMapping("/orderItem")
+public class OrderItemController {
+
+}
